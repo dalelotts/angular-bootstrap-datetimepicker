fa26d61c38a1cc385b535751d85314d9b12eb349
Dale Lotts <dlotts@knightrider.com>
2014-11-13 22:49:49 -0600

    Fix and add tests for on-before-render call back

--- test/configuration/beforeRender.spec.js
+++ test/configuration/beforeRender.spec.js
@@ -31,6 +31,32 @@ describe('beforeRender', function () {
   });
 
   describe('calls beforeRender before a new view is rendered', function () {
+    it('$dates parameter contains 42 members', function () {
+
+      $rootScope.date = moment("2008-01-01T00:00:00.000").toDate();
+      $rootScope.beforeRender = function (dates) {
+        expect(dates.length).toBe(0);
+      };
+
+      var postSpy = spyOn($rootScope, 'beforeRender');
+
+      var element = $compile('<datetimepicker data-ng-model=\'date\' data-before-render=\'beforeRender($dates)\' data-datetimepicker-config="{ startView: \'year\', minView: \'year\' }" ></datetimepicker>')($rootScope);
+      $rootScope.$digest();
+
+      var selectedElement = jQuery('.year', element)[2];
+
+      expect(selectedElement).hasClass('disabled');
+      selectedElement.trigger('click'); // No change if clicked!
+      expect($rootScope.date).toEqual(moment("2009-01-01T00:00:00.000").toDate());
+
+      expect(postSpy).toHaveBeenCalled();
+      expect(postSpy.mostRecentCall.args[0].length).toEqual(0);
+
+    });
+  });
+
+
+  describe('calls beforeRender before a new view is rendered', function () {
     it('year view and 2001 date is disabled', function () {
 
       $rootScope.date = moment("2008-01-01T00:00:00.000").toDate();
