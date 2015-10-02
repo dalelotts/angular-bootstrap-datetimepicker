abca8f2144e87f27ac7e55b28427984d25af2283
ekratiro <ekratiro@cisco.com>
2015-10-02 12:12:32 -0700

    adding dayFormat and dayPickFormat

--- src/js/datetimepicker.js
+++ src/js/datetimepicker.js
@@ -136,7 +136,9 @@
         '</table></div>',
         scope: {
           onSetTime: '&',
-          beforeRender: '&'
+          beforeRender: '&',
+          dayFormat: '=',
+          dayPickFormat: '='
         },
         replace: true,
         link: function link(scope, element, attrs, ngModelController) {
@@ -201,7 +203,7 @@
 
               var startDate = moment.utc(unixDate).startOf('year');
               var previousViewDate = startOfDecade(unixDate);
-              var activeDate = ngModelController.$modelValue ? moment(ngModelController.$modelValue).format('YYYY-MMM') : 0;
+              var activeDate = ngModelController.$modelValue ? moment(ngModelController.$modelValue).format("YYYY-MMM") : 0;
 
               var result = {
                 'previousView': 'year',
@@ -221,7 +223,7 @@
                 var dateValue = {
                   'utcDateValue': monthMoment.valueOf(),
                   'display': monthMoment.format('MMM'),
-                  'active': monthMoment.format('YYYY-MMM') === activeDate
+                  'active': monthMoment.format("YYYY-MMM") === activeDate
                 };
 
                 result.dates.push(new DateObject(dateValue));
@@ -247,7 +249,7 @@
                 'nextView': configuration.minView === 'day' ? 'setTime' : 'hour',
                 'previousViewDate': new DateObject({
                   utcDateValue: previousViewDate.valueOf(),
-                  display: startOfMonth.format('YYYY-MMM')
+                  display: startOfMonth.format("YYYY-MMM")
                 }),
                 'leftDate': new DateObject({utcDateValue: moment.utc(startOfMonth).subtract(1, 'months').valueOf()}),
                 'rightDate': new DateObject({utcDateValue: moment.utc(startOfMonth).add(1, 'months').valueOf()}),
@@ -257,7 +259,7 @@
 
 
               for (var dayNumber = 0; dayNumber < 7; dayNumber += 1) {
-                result.dayNames.push(moment.utc().weekday(dayNumber).format('dd'));
+                result.dayNames.push(moment.utc().weekday(dayNumber).format(dayFormat));
               }
 
               for (var i = 0; i < 6; i += 1) {
