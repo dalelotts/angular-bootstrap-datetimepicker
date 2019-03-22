e71233ad028af0732df960b7f5b89897daa097a9
Dale 'Ducky' Lotts <dlotts@knightrider.com>
2019-03-22 13:03:24 -0500

    feat(app): add configurator functionality to home page
    this will show when running `npm start`

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,17 +23,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'angular-bootstrap-datetimepicker'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Dale Lotts\' angular bootstrap date & time picker');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Dale Lotts\' angular bootstrap date & time picker!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,7 @@
  */
 
 import {Component} from '@angular/core';
-import {DateButton} from '../lib/dl-date-time-picker';
-import * as moment from 'moment';
-import {unitOfTime} from 'moment';
+import {DlDateTimePickerChange} from '../lib/dl-date-time-picker';
 
 @Component({
   selector: 'dl-root',
@@ -18,12 +16,68 @@ import {unitOfTime} from 'moment';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Dale Lotts\' angular bootstrap date & time picker';
-  startView = 'day';
+
+  maxView = 'year';
+  minView = 'minute';
   minuteStep = 5;
   selectedDate: Date;
-  selectFilter = (dateButton: DateButton, viewName: string) => {
-    const now = moment().startOf(viewName as unitOfTime.StartOf).valueOf();
-    return dateButton.value >= now;
+  showCalendar = true;
+  startView = 'day';
+  views = ['minute', 'hour', 'day', 'month', 'year'];
+
+  /**
+   * Sample implementation of a `change` event handler.
+   * @param event
+   *  The change event.
+   */
+
+  onCustomDateChange(event: DlDateTimePickerChange<Date>) {
+    console.log(event.value);
+  }
+
+  /**
+   * Determines whether or not the specified `minView` option is disabled (valid)
+   * considering the current `startView` and `maxView` settings.
+   * @param view
+   * the target view value.
+   */
+
+  isMinViewDisabled(view) {
+    return this.views.indexOf(view) > this.views.indexOf(this.startView)
+      || this.views.indexOf(view) > this.views.indexOf(this.maxView);
+  }
+
+  /**
+   * Determines whether or not the specified `maxView` option is disabled (valid)
+   * considering the current `startView` and `minView` settings.
+   * @param view
+   * the target view value.
+   */
+
+  isMaxViewDisabled(view) {
+    return this.views.indexOf(view) < this.views.indexOf(this.startView)
+      || this.views.indexOf(view) < this.views.indexOf(this.minView);
+  }
+
+  /**
+   * Determines whether or not the specified `startView` option is disabled (valid)
+   * considering the current `minView` and `maxView` settings.
+   * @param view
+   * the target view value.
+   */
+
+  isStartViewDisabled(view) {
+    return this.views.indexOf(this.minView) > this.views.indexOf(view)
+      || this.views.indexOf(this.maxView) < this.views.indexOf(view);
+  }
+
+  /**
+   * Removes/Adds the picker from the DOM forcing a re-render when
+   * the `starView` value changes.
+   */
+
+  refresh() {
+    this.showCalendar = false;
+    setTimeout(() => this.showCalendar = true, 100);
   }
 }
